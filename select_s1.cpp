14ce6273d4159af29f9ddeafaaf4c95673560060
이규민 <dlrbals719@gmail.com>
2025-05-28 11:37:09 +0900

    250528 class modified

--- select_s1.cpp
+++ select_s1.cpp
@@ -30,30 +30,68 @@ void select_s1::on_btn_search_clicked()
         return;
     }
 
-    QString dateStr = QString("%1-%2-%3")
-                          .arg(year)
-                          .arg(month.rightJustified(2, '0'))
-                          .arg(day.rightJustified(2, '0'));
+    QString dateStr;
+
+    if (year == "All")
+    {
+        dateStr = "";
+    }
+    else if (month == "All")
+    {
+        dateStr = QString("%1").arg(year);
+    }
+    else if (day == "All")
+    {
+        dateStr = QString("%1-%2")
+            .arg(year)
+            .arg(month.rightJustified(2, '0'));
+    }
+    else
+    {
+        dateStr = QString("%1-%2-%3")
+            .arg(year)
+            .arg(month.rightJustified(2, '0'))
+            .arg(day.rightJustified(2, '0'));
+    }
+
 
     SQLHSTMT hStmt;
-    if (SQLAllocHandle(SQL_HANDLE_STMT, hDbc, &hStmt) != SQL_SUCCESS) {
+    if (SQLAllocHandle(SQL_HANDLE_STMT, hDbc, &hStmt) != SQL_SUCCESS)
+    {
         print_error(SQL_HANDLE_STMT, hDbc);
         return;
     }
 
-    QString query = QString(
-                        "SELECT O.OrderDate, I.ItemName, O.PriceAtOrder, O.Quantity "
-                        "FROM USERS U, ORDERS O, ORDER_ITEM_DELIVERY OID, ITEMS I "
-                        "WHERE U.UserNo = O.UserNo "
-                        "AND O.OrderNo = OID.OrderNo "
-                        "AND OID.ItemNo = I.ItemNo "
-                        "AND U.UserName = '%1' "
-                        "AND O.OrderDate = '%2';")
-                        .arg(userName)
-                        .arg(dateStr);
+    QString query;
+
+    if (year == "All")
+    {
+        query = QString(
+                    "SELECT O.OrderDate, I.ItemName, O.PriceAtOrder, O.Quantity "
+                    "FROM USERS U, ORDERS O, ORDER_ITEM_DELIVERY OID, ITEMS I "
+                    "WHERE U.UserNo = O.UserNo "
+                    "AND O.OrderNo = OID.OrderNo "
+                    "AND OID.ItemNo = I.ItemNo "
+                    "AND U.UserName = '%1';")
+                    .arg(userName);
+    }
+    else
+    {
+        query = QString(
+                    "SELECT O.OrderDate, I.ItemName, O.PriceAtOrder, O.Quantity "
+                    "FROM USERS U, ORDERS O, ORDER_ITEM_DELIVERY OID, ITEMS I "
+                    "WHERE U.UserNo = O.UserNo "
+                    "AND O.OrderNo = OID.OrderNo "
+                    "AND OID.ItemNo = I.ItemNo "
+                    "AND U.UserName = '%1' "
+                    "AND O.OrderDate LIKE '%2%%';")
+                    .arg(userName)
+                    .arg(dateStr);
+    }
 
     SQLRETURN ret = SQLExecDirect(hStmt, (SQLCHAR*)query.toLocal8Bit().constData(), SQL_NTS);
-    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO) {
+    if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO)
+    {
         print_error(SQL_HANDLE_STMT, hStmt);
         SQLFreeHandle(SQL_HANDLE_STMT, hStmt);
         return;
@@ -66,14 +104,15 @@ void select_s1::on_btn_search_clicked()
     ui->tableWidget->setRowCount(0);
     ui->tableWidget->setColumnCount(colCount);
 
-    // 헤더
     QStringList headers = { "OrderDate", "ItemName", "PriceAtOrder", "Quantity" };
     ui->tableWidget->setHorizontalHeaderLabels(headers);
 
     int row = 0;
-    while (SQLFetch(hStmt) != SQL_NO_DATA) {
+    while (SQLFetch(hStmt) != SQL_NO_DATA)
+    {
         ui->tableWidget->insertRow(row);
-        for (SQLSMALLINT col = 0; col < colCount; ++col) {
+        for (SQLSMALLINT col = 0; col < colCount; ++col)
+        {
             SQLCHAR buffer[256];
             SQLGetData(hStmt, col + 1, SQL_C_CHAR, buffer, sizeof(buffer), nullptr);
             ui->tableWidget->setItem(row, col, new QTableWidgetItem(QString::fromLocal8Bit((char*)buffer)));
@@ -87,12 +126,15 @@ void select_s1::on_btn_search_clicked()
 
 void select_s1::initDateCombos()
 {
-    for (int year = 2019; year <= 2030; ++year)
+    ui->comboBox_year->addItem(QString("All"));
+    for (int year = 2019; year <= 2030; year++)
         ui->comboBox_year->addItem(QString::number(year));
 
-    for (int month = 1; month <= 12; ++month)
+    ui->comboBox_month->addItem(QString("All"));
+    for (int month = 1; month <= 12; month++)
         ui->comboBox_month->addItem(QString("%1").arg(month, 2, 10, QLatin1Char('0')));
 
-    for (int day = 1; day <= 31; ++day)
+    ui->comboBox_day->addItem(QString("All"));
+    for (int day = 1; day <= 31; day++)
         ui->comboBox_day->addItem(QString("%1").arg(day, 2, 10, QLatin1Char('0')));
 }
